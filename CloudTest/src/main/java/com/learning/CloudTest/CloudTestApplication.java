9d95ff0bfde7ee6509594c5930442ce46733c9e9
pavithra-55 <pavisribtech55@gmail.com>
2025-07-06 15:42:32 +0530

    Initial commit

---
+++ CloudTest/src/main/java/com/learning/CloudTest/CloudTestApplication.java
@@ -0,0 +1,13 @@
+package com.learning.CloudTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestApplication.class, args);
+	}
+
+}
---
+++ CloudTest/src/test/java/com/learning/CloudTest/CloudTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.CloudTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
